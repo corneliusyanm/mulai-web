219bb632ae246850563f356a4cc925059c827c77
corneliusyanm <onel.raja@gmail.com>
2025-02-23 09:32:10 +0700

    allow droplet ip

--- mulai_web/settings.py
+++ mulai_web/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'your-default-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['.mulai.id', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.mulai.id', 'localhost', '127.0.0.1', '178.128.116.170']
 
 # Application definition
 
