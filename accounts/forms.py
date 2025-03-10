9ae93b8e899d6c171850ea1d7c942d1a71e4b1d1
corneliusyanm <onel.raja@gmail.com>
2025-03-10 20:39:51 +0700

    improve form

--- accounts/forms.py
+++ accounts/forms.py
@@ -10,7 +10,20 @@ class MemberSignUpForm(forms.ModelForm):
             'goals', 'know_mulai_gym_from'
         ]
         widgets = {
-            'goals': forms.Textarea(attrs={'rows': 3}),
+            'goals': forms.Textarea(attrs={'rows': 2}),
+            'know_mulai_gym_from': forms.Textarea(attrs={'rows': 2}),
+            'height': forms.NumberInput(attrs={'step': '0.01', 'placeholder': 'Height in meters'}),
+            'weight': forms.NumberInput(attrs={'step': '0.1', 'placeholder': 'Weight in kg'}),
+        }
+        labels = { 
+            'height': 'Height (m)',
+            'weight': 'Weight (kg)',
+            'years_of_working_out': 'Years of Working Out',
+            'goals': 'Goals of Working Out',
+            'know_mulai_gym_from': 'How did you hear about Mulai Gym?',
+        }
+        help_texts = {
+            'know_mulai_gym_from': 'billboard, friends, instagram, google, etc.',
         }
 
 class MemberLoginForm(forms.Form):
