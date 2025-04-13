bb9144790f9e12372a0088d5740633bec2cd7f0b
corneliusyanm <onel.raja@gmail.com>
2025-04-13 17:40:44 +0700

    add /kuesioner

--- mulai_web/urls.py
+++ mulai_web/urls.py
@@ -14,17 +14,28 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 import visits.admin_init  # Add this import
 from visits.admin import admin_site  # Import the custom admin site
 
 urlpatterns = [
     # Use custom admin site instead of default
-    path('admin/', admin_site.urls),
-    path('', include('accounts.urls')),
-    path('', include('visits.urls')),
+    path("admin/", admin_site.urls),
+    path("", include("accounts.urls")),
+    path("", include("visits.urls")),
+    # Redirect /kuesioner to Google Form
+    path(
+        "kuesioner/",
+        RedirectView.as_view(
+            url="https://docs.google.com/forms/d/e/1FAIpQLSdilwD3u1paDqWNSasgA4a-kBYI9SO6UqWhd06bHOyRqAKsVw/viewform?usp=header",
+            permanent=False,
+        ),
+        name="kuesioner",
+    ),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
