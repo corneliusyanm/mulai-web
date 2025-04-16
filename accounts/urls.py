3f9c30c4d11458df135e7eb1946e5d33442ff8d9
corneliusyanm <onel.raja@gmail.com>
2025-04-16 19:02:40 +0700

    change accounts route to Indonesian

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("signup/", views.MemberSignUpView.as_view(), name="signup"),
-    path("signup/success/", views.signup_success, name="signup_success"),
-    path("login/", views.member_login, name="member_login"),
-    path("logout/", views.member_logout, name="member_logout"),
-    path("account/", views.MemberDetailView.as_view(), name="member_details"),
-    path("account/edit/", views.MemberEditView.as_view(), name="member_edit"),
+    path("daftar/", views.MemberSignUpView.as_view(), name="signup"),
+    path("daftar/berhasil/", views.signup_success, name="signup_success"),
+    path("masuk/", views.member_login, name="member_login"),
+    path("keluar/", views.member_logout, name="member_logout"),
+    path("akun/", views.MemberDetailView.as_view(), name="member_details"),
+    path("akun/ubah/", views.MemberEditView.as_view(), name="member_edit"),
 ]
