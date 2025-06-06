0fa82d67e279a5cbaf51c6236582c6ba08399234
corneliusyanm <onel.raja@gmail.com>
2025-06-06 17:52:41 +0700

    introduce pemula|semi_private|pt_active_member

--- accounts/models.py
+++ accounts/models.py
@@ -54,15 +54,18 @@ class Member(models.Model):
     age = models.IntegerField()
     height = models.DecimalField(max_digits=5, decimal_places=2)  # in cm
     weight = models.DecimalField(max_digits=5, decimal_places=2)  # in kg
-    address = models.TextField(blank=True, default="")  # Address field after weight
-    years_of_working_out = models.CharField(max_length=100)  # Changed to CharField
+    address = models.TextField(blank=True, default="")
+    years_of_working_out = models.CharField(max_length=100)
     goals = models.TextField()
     know_mulai_gym_from = models.CharField(max_length=255)
-    why_choose_mulai = models.TextField(
-        blank=True, default=""
-    )  # New field for why they chose Mulai
+    why_choose_mulai = models.TextField(blank=True, default="")
     created_at = models.DateTimeField(auto_now_add=True)
     active_until = models.DateTimeField(null=True, blank=True)
+    pemula_active_until = models.DateTimeField(null=True, blank=True)
+    semi_private_active_until = models.DateTimeField(null=True, blank=True)
+    pt_session_count = models.IntegerField(
+        default=0, help_text="Jumlah sesi personal training yang tersisa"
+    )
 
     def __str__(self):
         return f"{self.name} ({self.email})"
@@ -74,6 +77,26 @@ class Member(models.Model):
         # Consider active if end date is today or in the future
         today_start = timezone.now().replace(hour=0, minute=0, second=0, microsecond=0)
         return self.active_until >= today_start
+
+    @property
+    def is_pemula_active_member(self):
+        if not self.pemula_active_until:
+            return False
+        today_start = timezone.now().replace(hour=0, minute=0, second=0, microsecond=0)
+        return self.pemula_active_until >= today_start
+
+    @property
+    def is_semi_private_active_member(self):
+        if not self.semi_private_active_until:
+            return False
+        today_start = timezone.now().replace(hour=0, minute=0, second=0, microsecond=0)
+        return self.semi_private_active_until >= today_start
+
+    @property
+    def is_pt_active_member(self):
+        if not self.pt_session_count:
+            return False
+        return self.pt_session_count > 0
 
     class Meta:
         ordering = ["-created_at"]
